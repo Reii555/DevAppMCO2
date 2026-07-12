f0f53847aa8f039a4e7515f5e4bca63b44f46467
Rei <SRLagoos@gmail.com>
2026-07-12 19:53:58 +0800

    unfinished but i think dis is right

--- flight-reservation-server/server.js
+++ flight-reservation-server/server.js
@@ -1,1 +1,8 @@
+const connectDB = require('./database');
 
+require('dotenv').config(); // load ung environment variables
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+connectDB();
