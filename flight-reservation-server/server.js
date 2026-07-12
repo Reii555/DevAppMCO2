d60984aca4f952156f98acf6266dcde912521a5b
Rei <SRLagoos@gmail.com>
2026-07-13 02:30:44 +0800

    added consts

--- flight-reservation-server/server.js
+++ flight-reservation-server/server.js
@@ -1,4 +1,9 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const path = require('path');
 const connectDB = require('./database');
+const Users = require('./models/Users');
 
 require('dotenv').config(); // load ung environment variables
 
